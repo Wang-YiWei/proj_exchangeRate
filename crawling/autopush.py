f3661e0b560159ea7eed36eb1545e6c8568758fe
yiwei <wangyiwei11259@gmail.com>
2018-03-30 14:36:01 +0800

    Update data automatically at 2018-03-30 14:36:01

--- crawling/autopush.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import subprocess
-import datetime
-
-subprocess.call(["git", "add", "."])
-subprocess.call(["git", "commit", "-m", "Update data automatically at " + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))])
-subprocess.call(["git", "push"])
